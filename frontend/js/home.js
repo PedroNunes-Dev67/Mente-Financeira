126258eeab3759a9f8b1ec9b5c571a88f2146654
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2026-01-01 18:32:30 -0300

    Colocando links de navegação nos botoes

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,6 +1,37 @@
 const btnDespesa = document.getElementById('btnDespesa');
 const navSub = document.getElementById('navSub');
+const painelDireito = document.getElementById('painelDireito');
+
+const btnDashboard = document.getElementById('btnDashboard');
+const btnHistorico = document.getElementById('btnHistorico');
+const btnConfig = document.getElementById('btnConfig');
 
 btnDespesa.addEventListener('click', () => {
+
     navSub.classList.toggle('ativo');
+
+    const imgArrow = document.getElementById('imgArrow');
+
+     imgArrow.src = navSub.classList.contains('ativo')
+        ? '../assets/images/arrow-down-sign-to-navigate2.png'
+        : '../assets/images/arrow-down-sign-to-navigate.png';
+
+});
+
+btnDashboard.addEventListener('click' ,() => {
+
+    painelDireito.innerHTML = "<h1>Tela de Dashboard</h1>";
+
+});
+
+btnHistorico.addEventListener('click' ,() => {
+
+    painelDireito.innerHTML = "<h1>Tela de Historico</h1>";
+
+});
+
+btnConfig.addEventListener('click' ,() => {
+
+    painelDireito.innerHTML = "<h1>Tela de Configuração</h1>";
+
 });
