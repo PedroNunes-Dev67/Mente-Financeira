1da3c8e9c83faef3b68b45a4c46c35e8b86cd38c
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2026-01-02 16:06:47 -0300

    Criando prototipo da tela de Dashboard

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -23,36 +23,77 @@ btnDespesa.addEventListener('click', () => {
 
 btnDashboard.addEventListener('click' ,() => {
 
-    painelDireito.innerHTML = "<h1>Tela de Dashboard</h1>";
+    painelDireito.innerHTML = `<div id="cabecalho">
+                                <h1>Dashboard</h1>
+                              </div>
+                              <div class="dashPrincipal">
+                                <div class="dashDivEsq">
+                                    <div class="divEsqSon"></div>
+                                    <div class="divEsqSon"></div>
+                                </div>
+                                <div class="dashDivDir">
+                        
+                                </div>
+                            </div>`
 
 });
 
 btnHistorico.addEventListener('click' ,() => {
 
-    painelDireito.innerHTML = "<h1>Tela de Historico</h1>";
+    painelDireito.innerHTML = `<div id="cabecalho">
+                                <h1>Histórico</h1>
+                               </div>`
 
 });
 
 btnConfig.addEventListener('click' ,() => {
 
-    painelDireito.innerHTML = "<h1>Tela de Configuração</h1>";
+    painelDireito.innerHTML = `<div id="cabecalho">
+                                <h1>Configurações</h1>
+                               </div>`
+;
 
 });
 
 btnSuporte.addEventListener('click' ,() => {
 
-    painelDireito.innerHTML = "<h1>Tela de Suporte</h1>";
+    painelDireito.innerHTML = `<div id="cabecalho">
+                                <h1>Suporte</h1>
+                               </div>`
+;
 
 });
 
 navSub1.addEventListener('click' ,() => {
 
-    painelDireito.innerHTML = "<h1>Tela de Despesas Recorrentes</h1>";
+    painelDireito.innerHTML = `<div id="cabecalho">
+                                <h1>Despesas mensais</h1>
+                               </div>`
+;
 
 });
 
 navSub2.addEventListener('click' ,() => {
 
-    painelDireito.innerHTML = "<h1>Tela de Despesas não Recorrentes</h1>";
+    painelDireito.innerHTML = `<div id="cabecalho">
+                                <h1>Despesas cotidianas</h1>
+                               </div>`
+;
 
 });
+
+
+document.querySelectorAll('.links').forEach((e,i) => {
+
+    e.addEventListener('click', () => {
+
+        document.querySelectorAll('.links').forEach((ele, ind) => {
+            ele.classList.remove('btnClicado');
+        })
+
+        e.classList.add('btnClicado');
+        
+    })
+})
+
+
