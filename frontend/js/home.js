356147658ede0acd9230580a60012474f93a4ba8
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2026-01-02 13:59:32 -0300

    Melhorando tela de home

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -4,7 +4,10 @@ const painelDireito = document.getElementById('painelDireito');
 
 const btnDashboard = document.getElementById('btnDashboard');
 const btnHistorico = document.getElementById('btnHistorico');
+const btnSuporte = document.getElementById('btnSuporte');
 const btnConfig = document.getElementById('btnConfig');
+const navSub1 = document.getElementById('navSub1');
+const navSub2 = document.getElementById('navSub2');
 
 btnDespesa.addEventListener('click', () => {
 
@@ -33,5 +36,23 @@ btnHistorico.addEventListener('click' ,() => {
 btnConfig.addEventListener('click' ,() => {
 
     painelDireito.innerHTML = "<h1>Tela de Configuração</h1>";
+
+});
+
+btnSuporte.addEventListener('click' ,() => {
+
+    painelDireito.innerHTML = "<h1>Tela de Suporte</h1>";
+
+});
+
+navSub1.addEventListener('click' ,() => {
+
+    painelDireito.innerHTML = "<h1>Tela de Despesas Recorrentes</h1>";
+
+});
+
+navSub2.addEventListener('click' ,() => {
+
+    painelDireito.innerHTML = "<h1>Tela de Despesas não Recorrentes</h1>";
 
 });
