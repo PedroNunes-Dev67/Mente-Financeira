3119a4721954ae10a79e1ace3840ec783ff26dcf
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-01-26 21:36:40 -0300

    Criando classe de captura de exceções

---
+++ backend-springboot/MenteFinanceira/src/main/java/PedroNunesDev/MenteFinanceira/exception/model/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package PedroNunesDev.MenteFinanceira.exception.model;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
