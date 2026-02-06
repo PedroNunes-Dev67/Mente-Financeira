c94d3e2637e4cbeafac87de8008dd9f8616e84d5
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-02-05 22:04:21 -0300

    Refatorando classe Usuario implementando Lombok

--- backend-springboot/MenteFinanceira/src/main/java/PedroNunesDev/MenteFinanceira/model/Usuario.java
+++ backend-springboot/MenteFinanceira/src/main/java/PedroNunesDev/MenteFinanceira/model/Usuario.java
@@ -3,6 +3,10 @@ package PedroNunesDev.MenteFinanceira.model;
 import PedroNunesDev.MenteFinanceira.model.enums.UsuarioRole;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,14 +14,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Usuario implements UserDetails, Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_usuario")
@@ -31,81 +39,12 @@ public class Usuario implements UserDetails, Serializable {
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Despesa> despesas;
 
-    public Usuario() {
-    }
-
     public Usuario(String nome, String email, String senha, UsuarioRole role) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
         this.role = role;
         setVerificacaoEmail(false);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @JsonIgnore
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    @JsonIgnore
-    public UsuarioRole getRole() {
-        return role;
-    }
-
-    public void setRole(UsuarioRole role) {
-        this.role = role;
-    }
-
-    public boolean isVerificacaoEmail() {
-        return verificacaoEmail;
-    }
-
-    public void setVerificacaoEmail(boolean verificacaoEmail) {
-        this.verificacaoEmail = verificacaoEmail;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Usuario usuario = (Usuario) o;
-        return Objects.equals(id, usuario.id);
-    }
-
-    public List<Despesa> getDespesas() {
-        return despesas;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
     }
 
     @JsonIgnore
