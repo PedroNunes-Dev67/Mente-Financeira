6e20a5d018e799918bdb6a4e750999457bac8b77
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-03-18 13:44:27 -0300

    feat: criando Dockerfile e adicionando flyway a aplicação

--- backend-springboot/MenteFinanceira/src/main/java/PedroNunesDev/MenteFinanceira/config/InitializartionDataConfig.java
+++ backend-springboot/MenteFinanceira/src/main/java/PedroNunesDev/MenteFinanceira/config/InitializartionDataConfig.java
@@ -30,34 +30,6 @@ public class InitializartionDataConfig implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Categoria categoria1 = new Categoria("Saúde");
-        Categoria categoria2 = new Categoria("Alimentação");
-        Categoria categoria3 = new Categoria("Assinaturas");
-        Categoria categoria4 = new Categoria("Casa");
-        Categoria categoria5 = new Categoria("Compras");
-        Categoria categoria6 = new Categoria("Educação");
-        Categoria categoria7 = new Categoria("Lazer");
-        Categoria categoria8 = new Categoria("Serviços");
-        Categoria categoria9 = new Categoria("Supermercado");
-        Categoria categoria10 = new Categoria("Transporte");
-        Categoria categoria11 = new Categoria("Viagem");
-        Categoria categoria12 = new Categoria("Economias");
-        Categoria categoria13 = new Categoria("Outros");
 
-        categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2,categoria3,categoria4,categoria5,categoria6,categoria7,categoria8,categoria9,categoria10,
-                categoria11,categoria12,categoria13));
-
-        Role role1 = new Role("ROLE_USUARIO");
-        Role role2 = new Role("ROLE_ADMIN");
-
-        roleRepository.saveAll(Arrays.asList(role1,role2));
-
-        String senhaCripto = bCryptPasswordEncoder.encode("exemplo1234");
-
-        Usuario usuarioAdmin = new Usuario("Pedro Nunes", "exemplo@gmail.com", senhaCripto);
-        usuarioAdmin.getRoles().addAll(Arrays.asList(role1,role2));
-        usuarioAdmin.setVerificacaoEmail(true);
-
-        usuarioRepository.save(usuarioAdmin);
     }
 }
